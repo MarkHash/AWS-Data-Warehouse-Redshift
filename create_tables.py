e4e03dd29951624602ca8df18633a17d6bd55106
makotohashimoto <makoto.hashimoto06@gmail.com>
2021-11-01 10:50:25 +1100

    Added try and except

--- create_tables.py
+++ create_tables.py
@@ -7,18 +7,24 @@ def drop_tables(cur, conn):
     """
     Drops each table using the queries in drop_table_queries list. 
     """
-    for query in drop_table_queries:
-        cur.execute(query)
-        conn.commit()
+    try:
+        for query in drop_table_queries:
+            cur.execute(query)
+            conn.commit()
+    except Exception as e:
+        print(query, e)
 
 
 def create_tables(cur, conn):
     """
     Creates each table using the queries in create_table_queries list.
     """
-    for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
+    try:
+        for query in create_table_queries:
+            cur.execute(query)
+            conn.commit()
+    except Exception as e:
+        print(query, e)
 
 
 def main():
